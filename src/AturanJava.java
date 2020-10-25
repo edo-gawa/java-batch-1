a37286a5839254ef71e9123907e571f2ab20e381
edo <ahmad.ridwan@alfadigital.id>
2020-10-25 15:09:05 +0700

    update

---
+++ src/AturanJava.java
@@ -0,0 +1,18 @@
+
+public class AturanJava {//class
+
+	public static void main(String... argument) {//main function
+		
+		
+		System.out.println("Hello world");//code logic
+		/*  System.out.println adalah stream output dari java lang yang akan menuliskan semua yang berada di dalam tanda kurung () kedalam comand line atau shell semacam nya
+		 *   di dalam tanda kurung terdapat kutip 2  buka dan tutup, kutip 2 di java di baca sebagai string
+		 *   String bebas di isi apapun symbol ,angka dan huruf.
+		 *   jadi kesimpulan nya ystem.out.println akan menuliskan semua yang ada di dalam kutip 2 kedalam comand line atau shell
+		 *   di dalam kutip 2 terdapat kalimat Hello World, maka ketika file ini di jalan kan akan terdapat tulisan Hllo Word di dalam comand line atau shell yang di gunakan
+		 * 
+		 */
+	}//tutup function
+	
+}//tuutp class
+
