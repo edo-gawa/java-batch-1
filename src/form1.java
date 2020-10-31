d33acfe9fa28a0e03e360bf56af035e2775f4934
IlhamRayhandi <ilhamjunior22@gmail.com>
2020-10-31 16:18:52 +0700

    Latihan Sesi 2

---
+++ src/form1.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class form1 {
+
+	public static void main (String[] args){
+
+
+		Scanner input = new Scanner(System.in);
+		System.out.print("Nama : ");
+		String nama = input.next();
+		
+
+		input.skip("\r");
+		System.out.print("Umur : ");
+		int umur = input.nextInt();
+	
+
+		
+		System.out.print("Alamat : ");
+		String alamat = input.next();
+		
+
+		
+		System.out.print("Status : ");
+		String status = input.next();
+		
+
+	System.out.print("|==========================|");
+	System.out.println("\n| Nama Saya : " +nama);
+	System.out.println("|--------------------------|");
+	System.out.println("| Umur Saya : " +umur);
+	System.out.println("|--------------------------|");
+	System.out.println("| Alamat Saya : " +alamat);
+	System.out.println("|--------------------------|");
+	System.out.println("| Status Saya : " +status);
+	System.out.println("|==========================|");
+	input.close();
+	}
+}
